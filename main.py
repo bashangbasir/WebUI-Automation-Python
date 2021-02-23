85b89d9a4d9ea693c5a23a6141f3d56a166c5123
bashangbasir <bassam.abd.basir@gmail.com>
2021-02-24 01:24:43 +0800

    Update main.py
    Add sort function.
    
    Print result of the sorted list to the console.

--- main.py
+++ main.py
@@ -13,17 +13,29 @@ def setup (browser):
 
     #Implicit wait 
     driver.implicitly_wait(3)
+    #driver.maximize_windows()
     return driver  
 
 def teardown(driver):
     driver.quit()
 
+def sort_combined_result(results):
+    results.sort(key=getPrice)
+    return results
+
+def getPrice(list_content):
+    #return price 
+    return list_content[2]
+
+
 if __name__ == "__main__":
     
-    driver = setup("firefox")
+    driver = setup("chrome")
     
     amazon_page = AmazonPage(driver)
     amazon_page.load_page()
+    #need to change currency to MYR since amazon price in USD. 
+    amazon_page.change_currency_to_myr()
     amazon_page.search_item("iPhone 11")
     #get the list of the result that have been verified is iphone11 related
     amazon_results = amazon_page.get_searchresult()
@@ -34,7 +46,13 @@ if __name__ == "__main__":
     #get the list of the result that have been verified is iphone11 related
     ebay_results = ebay_page.get_searchresult()
     
-
+    all_results = amazon_results + ebay_results
+    sorted_results = sort_combined_result(all_results)
+    #use for debugging - see the price is sorted ascending order
+    for item in sorted_results:
+        print(item[2])
+    for item in sorted_results:
+        print(item)
     
-    #teardown(driver)
+    teardown(driver)
 
